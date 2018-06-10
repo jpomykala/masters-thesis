81856c88ca4ce36bfdece6e732a2963246bc9eae
jpomykala <evelan3094@gmail.com>
2018-06-10 17:31:58 +0200

    polishing

--- ClassifierApp/src/main.py
+++ ClassifierApp/src/main.py
@@ -27,44 +27,6 @@ vectorizer_1 = TfidfVectorizer(stop_words=stop_words_list, ngram_range=(5, 5), a
 vectorizer_2 = TfidfVectorizer(stop_words=stop_words_list, ngram_range=(6, 6), analyzer='char')
 vectorizer_3 = TfidfVectorizer(stop_words=stop_words_list, ngram_range=(7, 7), analyzer='char')
 
-
-# ft_clf = GensimFastText(dim=7, min_count=15, loss='ns', epoch=200, bucket=200000, word_ngrams=1)
-
-
-def show_support_matrix(korpus_name, target_names, ft_support, nb_support, svm_support, dt_support):
-    classes = target_names
-    data = np.array([ft_support, nb_support, svm_support, dt_support])
-    data = np.transpose(data)
-
-    shape = (len(classes), 4)
-    matrix_data = data.reshape(shape)
-
-    categories = ['fastText', 'NaiveBayes', 'SVM', 'Drzewo decyzyjne']
-
-    fig, ax = plt.subplots()
-    im = ax.imshow(matrix_data)
-
-    ax.set_xticks(np.arange(len(categories)))
-    ax.set_yticks(np.arange(len(classes)))
-
-    ax.set_xticklabels(categories)
-    ax.set_yticklabels(classes)
-
-    ax.set_aspect('auto')
-
-    for i in range(len(classes)):
-        for j in range(len(categories)):
-            text = ax.text(j, i, matrix_data[i, j],
-                           ha="center", va="center", color="white")
-
-    title = 'Support - ' + korpus_name
-    ax.set_title(title)
-    fig.tight_layout()
-    plt.savefig(plot_save_path + title.lower().replace(' ', '-') + "." + plotFormat, dpi=dpi,
-                format=plotFormat)
-    plt.show()
-
-
 def show_confusion_matrix(y_test, y_pred, class_names, title, korpus_name):
     cnf_matrix = confusion_matrix(y_test, y_pred)
     plt.figure()
@@ -95,7 +57,7 @@ def draw_predict_time_plot(ax_samples, korpus_name):
     plt.plot(list(reversed(ax_samples)), list(reversed(ft_predict_times)), 'c-+', label="FastText")
     plt.plot(list(reversed(ax_samples)), list(reversed(nb_predict_times)), 'r-*', label="NaiveBayes")
     plt.plot(list(reversed(ax_samples)), list(reversed(svm_predict_times)), 'g-^', label="SVM")
-    plt.plot(list(reversed(ax_samples)), list(reversed(dt_predict_times)), 'b-s', label="Decision Tree")
+    plt.plot(list(reversed(ax_samples)), list(reversed(dt_predict_times)), 'b-s', label="Drzewo decyzyjne")
     plt.grid(color='tab:gray', linestyle='-', linewidth=0.15)
     plt.ylabel('czas [s]')
     plt.xlabel('liczba próbek')
@@ -107,7 +69,7 @@ def draw_predict_time_plot(ax_samples, korpus_name):
 
 
 def draw_time_plot(ax_samples, korpus_name):
-    plt.plot(ax_samples, ft_times, 'c-+', label="FastText")
+    plt.plot(ax_samples, ft_times, 'c-+', label="fastText")
     plt.plot(ax_samples, nb_times, 'r-*', label="NaiveBayes")
     plt.plot(ax_samples, svm_times, 'g-^', label="SVM")
     plt.plot(ax_samples, dt_times, 'b-s', label="Drzewo decyzyjne")
@@ -122,7 +84,7 @@ def draw_time_plot(ax_samples, korpus_name):
 
 
 def draw_accuracy_plot(ax_samples, korpus_name):
-    plt.plot(ax_samples, ft_accuracies, 'c-+', label="FastText")
+    plt.plot(ax_samples, ft_accuracies, 'c-+', label="fastText")
     plt.plot(ax_samples, nb_accuracies, 'r-*', label="NaiveBayes")
     plt.plot(ax_samples, svm_accuracies, 'g-^', label="SVM")
     plt.plot(ax_samples, dt_accuracies, 'b-s', label="Drzewo decyzyjne")
@@ -134,22 +96,6 @@ def draw_accuracy_plot(ax_samples, korpus_name):
     plt.legend()
     plt.savefig(plot_save_path + title.lower().replace(' ', '-') + "." + plotFormat, dpi=dpi, format=plotFormat)
     plt.show()
-
-
-def draw_fastText_plot(ax_samples, title):
-    plt.plot(ax_samples, fastText_accuracies_1, 'r-*', label="epoch = 5")
-    plt.plot(ax_samples, fastText_accuracies_2, 'g-^', label="epoch = 50")
-    plt.plot(ax_samples, fastText_accuracies_3, 'b-s', label="epoch = 200")
-    plt.plot(ax_samples, fastText_accuracies_4, 'm-h', label="epoch = 500")
-    plt.plot(ax_samples, fastText_accuracies_5, 'c-+', label="epoch = 1000")
-    plt.grid(color='tab:gray', linestyle='-', linewidth=0.15)
-    plt.ylabel('dokładność')
-    plt.xlabel('liczba próbek')
-    plt.title(title)
-    plt.legend()
-    plt.savefig(plot_save_path + title.lower().replace(' ', '-') + "." + plotFormat, dpi=dpi, format=plotFormat)
-    plt.show()
-
 
 def draw_bow_plot(ax_samples, korpus_name):
     plt.plot(ax_samples, bow_1, 'r-*', label="ngram = 5")
@@ -272,10 +218,10 @@ def accuracy_time_report(train_sizes, iterations, korpus_path, korpus_name):
         dt_roc_auc_score_array.append(dt_roc_auc)
 
         # draw plots
-        # draw_accuracy_plot(train_samples_array, korpus_name)
-        # draw_fit_time_plot(train_samples_array, korpus_name)
-        # draw_predict_time_plot(test_samples_array, korpus_name)
-        # draw_time_plot(train_samples_array, korpus_name)
+        draw_accuracy_plot(train_samples_array, korpus_name)
+        draw_fit_time_plot(train_samples_array, korpus_name)
+        draw_predict_time_plot(test_samples_array, korpus_name)
+        draw_time_plot(train_samples_array, korpus_name)
 
         draw_bow_plot(train_samples_array, korpus_name)
 
